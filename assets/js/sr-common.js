0cf1d8f8ec332beef2526249de20bbf01f4321f9
HisashiMiya <korokorokororintyo@gmail.com>
2026-02-01 14:14:40 +0900

    Add files via upload

--- assets/js/sr-common.js
+++ assets/js/sr-common.js
@@ -88,3 +88,12 @@
     boot();
   }
 })();
+
+// --- GA4 init (safe) ---
+(function initGA4() {
+  window.dataLayer = window.dataLayer || [];
+  window.gtag = window.gtag || function(){ window.dataLayer.push(arguments); };
+
+  window.gtag('js', new Date());
+  window.gtag('config', 'G-XPMXMPVEKV');
+}
